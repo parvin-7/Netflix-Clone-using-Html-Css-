12f91f0b663c43cb89de429612206e2ffa16f56b
Parvindar Singh <parvindarsingh43958@gmail.com>
2024-01-13 22:13:33 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const faq = document.querySelectorAll(".accordian");
+// console.log(faq);
+
+faq.forEach(accordian =>{
+    const icon = accordian.querySelector("#icon");
+    const answer = accordian.querySelector(".answer");
+
+    accordian.addEventListener("click",()=>{
+        icon.classList.toggle("active");
+        answer.classList.toggle("active"); 
+    })
+})
